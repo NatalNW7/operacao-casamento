856d8312fd3c8f686f093f077f01f3bcecc1e9c0
NatalNW7 <natanaelweslley1@gmail.com>
2025-08-29 18:02:37 -0300

    refactor: return 201 when recieve a webhook

--- app/api/payment/webhook/route.ts
+++ app/api/payment/webhook/route.ts
@@ -39,7 +39,7 @@ export async function POST(request: NextRequest) {
       }
     });
 
-    return NextResponse.json({message: "ok"});
+    return NextResponse.json({message: "ok"}, { status: 201 });
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
     return NextResponse.json({ error: errorMessage }, { status: 500 });
